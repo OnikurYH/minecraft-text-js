cddf483eddddb4f576cd5ed08962ffd053fdcb54
OnikurYH <onikuryh@gmail.com>
2016-11-20 23:29:47 +0800

    Make minify version for browser

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,20 @@
 const gulp = require("gulp")
 const babel = require("gulp-babel")
-const eslint = require("gulp-eslint");
+const eslint = require("gulp-eslint")
+const uglify = require("gulp-uglify")
+const rename = require("gulp-rename")
 
 gulp.task("js", function () {
-  return gulp.src("src/**/*.js")
+  return gulp.src("src/minecraft_text.js")
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError())
   .pipe(babel({
     presets: ["es2015"]
   }))
+  .pipe(gulp.dest("dist"))
+  .pipe(uglify())
+  .pipe(rename({ extname: '.min.js' }))
   .pipe(gulp.dest("dist"));
 })
 
